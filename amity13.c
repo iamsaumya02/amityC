74b8cb894732370ed559a706157d98a5462a238f
iamsaumya02 <summasrivastava@gmail.com>
2020-10-10 20:59:23 +0530

    C

---
+++ amity13.c
@@ -0,0 +1,17 @@
+  
+  
+   #include<stdio.h>
+   void main()
+   {
+	   int n,r,rev=0;
+	   printf("Enter the number:");
+	   scanf("%d",&n);
+	   while(n>0)
+	   {
+		   r=n%10;
+		   rev=rev*10+r;
+		   n=n/10;
+	   }
+	     printf("Reverse of the number= %d",rev);
+   }
+		   
